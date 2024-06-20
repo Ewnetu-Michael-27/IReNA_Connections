bfadc23c9e2f56b45ddb02174a127138d7f3431f
Michael Ewnetu <143202037+Ewnetu-Michael-27@users.noreply.github.com>
2024-06-20 16:16:06 -0400

    Update 2 IReNA Insight.py

--- pages/2 IReNA Insight.py
+++ pages/2 IReNA Insight.py
@@ -1,9 +1,9 @@
 import streamlit as st 
 import pandas as pd 
 import numpy as np 
-import pydeck as pdk 
 import pickle
 import plotly.graph_objs as go
+import networkx as nx 
 
 
 st.set_page_config(
@@ -78,4 +78,163 @@ with col10:
 st.write("")
 st.markdown("***")
 st.write("")
+
+st.header("Focus Area Membership")
+st.write("")
+st.markdown(""" 
+IReNA is organized into eight focus areas (FA) that address 
+            problem at the forefront of science. In all eight areas there is a particularly strong benefit of connecting the 
+            complementary expertise and capabilities of the various international networks and coordinate research internationally. """)
+
+st.write("")
+st.write("YRO: Young Researchers Organization")
+st.write("FA1: Nuclear Reaction Rates")
+st.write("FA2: Stellar Abundances")
+st.write("FA3: Dense Matter in Supernovae and Neutron Star Mergers")
+st.write("FA4: r-process Experiments")
+st.write("FA5: Computer Models")
+st.write("FA6: Nuclear Data for Astrophysics")
+st.write("FA7: Weak Interactions")
+st.write("FA8: Professional Development and Broadening Participation")
+st.write("")
+
+data_2=pd.read_csv("data_06_19.csv")
+
+
+FA=["YRO?","FA1","FA2","FA3","FA4","FA5","FA6","FA7","FA8"]
+option_10A=st.selectbox("Choose Focus Area from the following options to see membership distribution", FA)
+
+result=data_2[data_2[option_10A]==1]
+
+ids=list(result["ID"])
+lats=list(result["Latitude"])
+lons=list(result["Longitude"])
+Net=list(result["Network"])
+
+graph=nx.Graph()
+
+dict_by_id={}
+
+for i in range(len(ids)):
+    id_tempo=ids[i]
+    tempo=[]
+
+    if dict_by_id.get(id_tempo,0)!=0:
+        continue
+    else:
+        tempo.append(Net[i])
+        tempo.append(lats[i])
+        tempo.append(lons[i])
+        
+        dict_by_id[id_tempo]=tempo
+
+for i in ids:
+    net=dict_by_id[i][0]
+    graph.add_node(i,Network=net)
+
+nodes_list=list(graph.nodes())
+
+for i in range(0,len(nodes_list),1):
+    for j in range(i+1,len(nodes_list),1):
+        if graph.nodes[nodes_list[i]]["Network"] != graph.nodes[nodes_list[j]]["Network"]:
+            graph.add_edge(nodes_list[i], nodes_list[j])
+        else:
+            continue
+
+
+
+sd=0.01
+
+lats=[sum(x) for x in zip(lats, list(np.random.normal(0, sd, len(lats))))]
+lons=[sum(x) for x in zip(lons, list(np.random.normal(0, sd, len(lons))))]
+     
+#dpcapacity = [nl[1]['dpcapacity'] for  nl in node_data]
+tooltips = ids
+
+#Define the dict for the graph layout (i.e. node geographic locations):
+pos = {id: [lo, la]  for id, lo, la in zip(ids, lons, lats)}
+
+#Get the map center:
+x_center = (min(lons) + max(lons))/2
+y_center = (min(lats) + max(lats))/2
+
+edge_list = list(graph.edges(data=True))
+
+pl_edges = [(item[0], item[1]) for item in edge_list]
+
+coords=[]
+for e in pl_edges:
+    coords.append([ [pos[e[0]][0], pos[e[0]][1]],  [pos[e[1]][0], pos[e[1]][1]] ])  
+
+#Assign color
+network_color_map={
+    'EMMI':'#2ca02c', #green
+    'CeNAM':'#9467bd', #Purple 
+    'UKAKUREN':'#bcbd22', #yellow-green
+    'NuGRID':'#17becf', #cyan
+    'ChETEC Infra':'#ff7f0e', #Orange
+    'ChETEC':'#e377c2', #pink
+    'BRIDGCE':'#7f7f7f', #gray
+    'CaNPAN':'#98df8a', #light green
+    'CRC881':'#ffbb78',#light Orange 
+    'JINA-CEE':'#1f77b4', #blue
+    'IANNA':'yellow', #yellow
+    'ChETEC, NuGRID': '#8c564b' #brown
+}
+
+node_colors=[network_color_map[net] for net in Net]
+
+fig_1= go.Figure()
+
+for network in network_color_map:
+    net_ids = [ids[i] for i in range(len(Net)) if Net[i] == network]
+    net_lats = [lats[i] for i in range(len(Net)) if Net[i] == network]
+    net_lons = [lons[i] for i in range(len(Net)) if Net[i] == network]
+    net_color = network_color_map[network]
+    
+    fig_1.add_trace(go.Scattermapbox(
+        lat=net_lats,
+        lon=net_lons,
+        mode='markers',
+        text=net_ids,
+        marker=dict(size=8, color=net_color),
+        name=network,
+        showlegend=True,
+        hoverinfo='text'
+    ))
+
+layers = [dict(sourcetype = 'geojson',
+               source={"type": "Feature",
+                       "geometry": {"type": "MultiLineString",
+                                    "coordinates": coords}
+                      },
+             color= 'red',
+             type = 'line',
+              opacity=0.015,   
+             line=dict(width=1),
+            )]
+
+#mapboxt = open(".mapbox_token").read().rstrip() #my mapbox_access_token  must be set only for special mapbox style
+fig_1.update_layout(title_text="IReNA Members in "+option_10A+" Connected Across Network", title_x=0.5,
+              font=dict(family='Balto', color='black'),
+              autosize=False,
+              width=1200,
+              height=1200,
+              hovermode='closest',
+    
+              mapbox=dict(#accesstoken=mapbox_access_token,
+                          layers=layers,
+                          bearing=0,
+                          center=dict(lat=y_center,
+                                      lon=x_center+0.01),
+                          pitch=0,
+                          zoom=0.3,
+                          style='open-street-map'
+                         ),
+            margin=dict(t=150)
+            )
+
+if st.button("Click to see chart"):
+    st.write(f"**{len(ids)}** members in **{option_10A}**")
+    st.plotly_chart(fig_1)
 
